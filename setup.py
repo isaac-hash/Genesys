9507399db046d5c28c3045630eac010791e28bc9
isaac-hash <zikkychukwudulue@gmail.com>
2025-09-11 23:06:08 +0100

    added setup file for easy pip installation

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='genesys-framework',
+    version='0.1.0',
+    # find_packages() will automatically discover `framework_cli` and `framework_core`
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        # rclpy and other ROS packages are expected to be in the sourced environment
+    ],
+    entry_points={
+        'console_scripts': [
+            # This creates the `framework` command and points it to your main function
+            'framework = framework_cli.main:cli',
+        ],
+    },
+    author='Genesys Developer',
+    author_email='dev@genesys.ros',
+    description='A developer-friendly, opinionated framework for ROS 2.',
+    license='Apache 2.0',
+)
