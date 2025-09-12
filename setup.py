02d4908ace2a598356b161f508a5c88b01cd07ef
isaac-hash <zikkychukwudulue@gmail.com>
2025-09-12 14:58:30 +0100

    fixed the command name registering issue

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='genesys-framework',
+    name='genesys-framework-cli',
     version='0.1.0',
     # find_packages() will automatically discover `framework_cli` and `framework_core`
     packages=find_packages(),
