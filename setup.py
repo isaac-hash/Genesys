279b5f4a1e369c8e9e58ddcaa357349072c89e92
isaac-hash <zikkychukwudulue@gmail.com>
2025-09-15 08:43:25 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='genesys-framework-cli',
-    version='0.1.0',
+    version='0.1.1',
     # find_packages() will automatically discover `framework_cli` and `framework_core`
     packages=find_packages(),
     install_requires=[
